2353a22d8b192c5c21a27f5eac24c0a6ed283375
Linch2126 <101205991+Linch2126@users.noreply.github.com>
2022-03-09 16:49:54 +0100

    Blind Token

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
-client.login("OTUwNzAxMjk0OTQ1OTcyMjQ2.Yicvkw.Ou8yY_25BSqTd4K6UUFj5nZpdvw")
+client.login(process.env.token)
 
 client.on("ready", () => {
     console.log("Bot Online")

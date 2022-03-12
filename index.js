5d9b1c48788876aaf98a28cc7711719fd626828c
Linch2126 <101205991+Linch2126@users.noreply.github.com>
2022-03-12 14:06:51 +0100

    Command new 1

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const Discord = require("discord.js")
-const client = new Discord.Client({intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]})
-client.login(process.env.token)
-client.on("ready", () => {console.log("Bot Online")})
+const { Client, Intents } = require('discord.js');
+const wait = require('util').promisify(setTimeout);
 
-client.on('messageCreate', async (message) =>{
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Bot online!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.on('messageCreate', async (message) => {
     if (!client.application?.owner) {
         await client.application?.fetch();
     }
 
-    if (message.content.tolowerCase() === '$registra' && message.author.id === client.application?.owner.id) {
-        
+    if (message.content.toLowerCase() === '!registra' && message.author.id === client.application?.owner.id) {
+        const data = {
+            name: 'ping',
+            description: 'Risponde con pong!',
+        }
+
+        // Registra un comando GLOBALE
+        // const comando = await client.application?.commands.create(data);
+        // console.log(comando);
+
+        // Registra un comando GUILD
+        const comando = await client.guilds.cache.get('871746640418066474')?.commands.create(data);
+        console.log(comando);
     }
 })
+
+client.login('OTUwNzAxMjk0OTQ1OTcyMjQ2.Yicvkw.CIkJdkfLAZxyBF-b9B0RgrN-tcA');

5acf900f4d9fe2ebcf9046de5546549ee8a8522e
Linch2126 <101205991+Linch2126@users.noreply.github.com>
2022-03-13 13:33:00 +0100

    Big Update

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ client.on("messageCreate", (message) => {
             .addField("$file", "il bot manderà un file", true)
             .addField("$embed", "per mandare un embed", true)
             .addField("$spam", "per mandare messaggi di spam", true)
+            .addField("$clear", "per eliminare un tot di messaggi", true)
+            .addField("$serverinfo", "per vedere tutte le info sul server", true) 
+            .addField("$userinfo", "per vedere tutte le info su un utente", true)
             .setFooter("Eccoti la lista dei comandi!")
             .setTimestamp();
 
@@ -75,3 +78,123 @@ client.on("messageCreate", (message) => {
 })
 
 
+
+
+//Comandi Complessi
+
+client.on("guildMemberAdd", (member) => {
+    client.channels.cache.get("952542863810506872").send("Benvenuto " + member.toString() + " in " + member.guild.name + ", sei il " + member.guild.member.Count + "° membro");
+})
+
+client.on("message", (message) => {
+    if(message.content.startsWith("$clear")){
+        if(!message.member.hasPermission("MANAGE_MESSAGES")){
+            message.channel.send("Non hai il permesso");
+            return;
+        }
+        if(!message.guild.me.hasPermission("MANAGE_MESSAGES")){
+            message.channel.send("Non ho il permesso")
+            return
+        }
+
+        var count = message.content.slice(7);
+        count = parseInt(count);
+
+        if(!count){
+            message.channel.send("Inserisci un numero valido");
+            return
+        }
+
+        message.channel.bulkDelete(count, true);
+        message.channel.send("Ho eliminato " + count + " messaggi")
+        .then(msg =>{
+            msg.delete({ timeout:2000 })
+        })
+    }
+})
+
+
+
+
+
+
+client.on("message", message => {
+    if (message.content == "$serverinfo") {
+        var server = message.member.guild;
+
+        var botCount = server.members.cache.filter(member => member.user.bot).size;
+        var utentiCount = server.memberCount - botCount;
+
+        var categoryCount = server.channels.cache.filter(c => c.type == "category").size
+        var textCount = server.channels.cache.filter(c => c.type == "text").size
+        var voiceCount = server.channels.cache.filter(c => c.type == "voice").size
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle(server.name)
+            .setDescription("Tutte le info su questo server")
+            .setThumbnail(server.iconURL())
+            .addField("Owner", "```" + server.owner.user.username + "```", true)
+            .addField("Server id", "```" + server.id + "```", true)
+            .addField("Server region", "```" + server.region + "```", true)
+            .addField("Members", "```Total: " + server.memberCount + " - Users: " + utentiCount + " - Bots: " + botCount + "```", false)
+            .addField("Channels", "```Category: " + categoryCount + " - Text: " + textCount + " - Voice: " + voiceCount + "```", false)
+            .addField("Server created", "```" + server.createdAt.toDateString() + "```", true)
+            .addField("Boost level", "```Level " + server.premiumTier + " (Boost: " + server.premiumSubscriptionCount + ")```", true)
+
+        message.channel.send(embed)
+
+    }
+});
+
+
+
+
+
+client.on("message", message => {
+    if (message.content.startsWith("$userinfo")) {
+        if (message.content == "$userinfo") {
+            var utente = message.member;
+        }
+        else {
+            var utente = message.mentions.members.first();
+        }
+
+        if (!utente) {
+            message.channel.send("Non ho trovato questo utente")
+            return
+        }
+
+        var elencoPermessi = "";
+        if (utente.hasPermission("ADMINISTRATOR")) {
+            elencoPermessi = "👑 ADMINISTRATOR";
+        }
+        else {
+            var permessi = ["CREATE_INSTANT_INVITE", "KICK_MEMBERS", "BAN_MEMBERS", "MANAGE_CHANNELS", "MANAGE_GUILD", "ADD_REACTIONS", "VIEW_AUDIT_LOG", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "VIEW_GUILD_INSIGHTS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "MOVE_MEMBERS", "USE_VAD", "CHANGE_NICKNAME", "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"]
+
+            for (var i = 0; i < permessi.length; i++) {
+                if (utente.hasPermission(permessi[i])) {
+                    elencoPermessi += "- " + permessi[i] + "\r";
+                }
+            }
+        }
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle(utente.user.tag)
+            .setDescription("Tutte le info di questo utente")
+            .setThumbnail(utente.user.avatarURL())
+            .addField("User id", "```" + utente.user.id + "```", true)
+            .addField("Status", "```" + utente.user.presence.status + "```", true)
+            .addField("Is a bot?", utente.user.bot ? "```Yes```" : "```No```", true)
+            .addField("Account created", "```" + utente.user.createdAt.toDateString() + "```", true)
+            .addField("Joined this server", "```" + utente.joinedAt.toDateString() + "```", true)
+            .addField("Permissions", "```" + elencoPermessi + "```", false)
+            .addField("Roles", "```" + utente.roles.cache.map(ruolo => ruolo.name).join("\r") + "```", false)
+
+        message.channel.send(embed)
+
+    }
+});
+
+
+
+/* By Linch#2126 */
